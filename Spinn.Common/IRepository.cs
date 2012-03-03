cb21ab756736c95bc1e1b4b41a45f940b51dfaf7
badwah <pieter.clavie@gmail.com>
2012-03-03 17:28:27 +0100

    Model V2

---
+++ Spinn.Common/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Spinn.Common
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T newItem);
+        void Update(T newItem);
+
+    }
+}
